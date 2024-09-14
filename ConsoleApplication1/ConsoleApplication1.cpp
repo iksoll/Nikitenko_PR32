fc91a62e18e2ff23beb3d3b31201ce74549fdfa5
iksoll <157449149+iksoll@users.noreply.github.com>
2024-09-14 12:18:39 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num, sum=0,digit;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    while (num > 0) {
+        digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    }
+    std::cout << "Сумма цифр числа: ";
+    std::cout << sum;
+
+
+    int x, n, h;
+    std::cout << "Введите начальное число: ";
+    std::cin >> x;
+    std::cout << "Введите шаг геометрической прогрессии: ";
+    std::cin >> h;
+    std::cout << "Введите конечное число: ";
+    std::cin >> n;
+    for (int i = x; i <= n; i = i * h) {
+        std::cout << i;
+        std::cout << " ";
+    }
+}
